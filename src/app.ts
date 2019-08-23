6c1b2b330354361c749eafdb371a268553705885
unknown <413295894@qq.com>
2019-08-23 16:16:17 +0800

    koa-cors

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,17 @@ import Token from './middlewares/token';
 const app = new Koa();
 const router = new KoaRouter();
 
+//跨域中间件
+app.use(async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    if (ctx.method == 'OPTIONS') {
+        ctx.body = 200;
+    } else {
+        await next();
+    }
+});
 
 //Jwt配置
 app.use(KoaJwt({ secret: Token.jwtSecret, passthrough: true, key: Token.jwtSecret }).unless({ path: [/^\/public/] }));
